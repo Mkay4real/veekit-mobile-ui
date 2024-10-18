2fb06df5728a04f4819af843ab1b28edc2a73315
Mukhtar Oyelayo <mukhtaroyelayo@gmail.com>
2024-10-18 08:10:19 +0100

    chore: initial commit

---
+++ android/src/main/java/com/veekitmobileui/VeekitMobileUiPackage.kt
@@ -0,0 +1,17 @@
+package com.veekitmobileui
+
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ViewManager
+
+
+class VeekitMobileUiPackage : ReactPackage {
+  override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+    return listOf(VeekitMobileUiModule(reactContext))
+  }
+
+  override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+    return emptyList()
+  }
+}
---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  VeekitMobileUiExample
+//
+
+import Foundation
