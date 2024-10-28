8b92005314239b37c9bd6349db26a51e27145da9
Mukhtar Oyelayo <mukhtaroyelayo@gmail.com>
2024-10-28 17:45:19 +0100

    chore(icon): WLVA-95: wip - set up SVG and icon props

---
+++ src/icons/types.ts
@@ -0,0 +1,8 @@
+export type IconSize = 16 | 20 | 24 | 40 | 48
+export type IconProps = {
+  name?: string
+  color?: string
+  size?: IconSize
+  accessibilityLabel?: string
+  accessibilityHint?: string
+}
