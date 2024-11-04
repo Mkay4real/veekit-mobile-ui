ad4958bda10db2eb93ab0a9a392f66518ce7cd84
Mukhtar Oyelayo <mukhtaroyelayo@gmail.com>
2024-11-04 13:54:55 +0100

    chore(UI): WLVA-86: Update the File Input component

--- src/components/ui/file-input.tsx
+++ src/components/ui/file-input.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, Alert } from 'react-native';
 import DocumentPicker, { type DocumentPickerResponse } from 'react-native-document-picker';
-// import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import tw from '../../lib/tailwind';
+import CloudIcon from '../../icons/cloud';
+import TrashIcon from '../../icons/trash';
 
 interface FileInputProps {
   onFileSelect?: (file: DocumentPickerResponse | null) => void;
@@ -22,6 +23,7 @@ const FileInput: React.FC<FileInputProps> = ({
       const result = await DocumentPicker.pick({
         //@ts-ignore 
         type: acceptedFormats.map(format => DocumentPicker.types[format.toUpperCase()] || format),
+        // type: DocumentPicker.types.images,// acceptedFormats.map(format => DocumentPicker.types[format.toUpperCase()] || format),
         copyTo: 'cachesDirectory',
       });
       const file = result[0] || null;
@@ -46,44 +48,42 @@ const FileInput: React.FC<FileInputProps> = ({
 
   return (
     <View style={tw`w-full space-y-2`}>
-      <Text style={tw`text-purple-600 text-lg font-semibold`}>
-        {/* <Icon name="upload" size={20} color="#9333EA" /> */}
-        Upload Document
-      </Text>
-      <View style={tw`border-2 border-dashed border-gray-200 rounded-lg p-6`}>
+      <View style={tw`border-2 border-dashed border-gray-200 self-center max-w-[80%] rounded-lg p-6`}>
         {!selectedFile ? (
           <View style={tw`items-center space-y-4`}>
-            <View style={tw`bg-purple-50 p-4 rounded-full`}>
-              {/* <Icon name="cloud-upload" size={32} color="#9333EA" /> */}
-            </View>
+              <CloudIcon />
+           
             <TouchableOpacity
               onPress={handleFilePick}
-              style={tw`bg-white border border-gray-300 rounded-lg px-6 py-2`}
+              style={tw`bg-white border my-4 border-gray-300 rounded-lg px-6 py-2`}
             >
               <Text style={tw`text-gray-700 font-medium`}>Browse file</Text>
             </TouchableOpacity>
-            <Text style={tw`text-gray-500 text-sm`}>
+            <Text style={tw`text-gray-500 text-sm-body`}>
               Accepted file format: {acceptedFormats.join(', ').toUpperCase()}
             </Text>
           </View>
         ) : (
-          <View style={tw`bg-gray-50 rounded-lg p-4 flex-row justify-between items-center`}>
-            <View style={tw`flex-row items-center flex-1`}>
+          <View style={tw`rounded-lg p-4 justify-between items-center`}>
+            <View style={tw`items-center justify-center`}>
+                <CloudIcon />
               {/* <Icon name="file-document" size={24} color="#4B5563" /> */}
-              <View style={tw`ml-3`}>
+              <View style={tw`items-center`}>
                 <Text style={tw`text-gray-900 font-medium`}>
                   {selectedFile.name}
                 </Text>
-                <Text style={tw`text-gray-500 text-sm`}>
+                <Text style={tw`text-gray-500 text-sm-body`}>
                   {(selectedFile.size || 0 / 1024).toFixed(0)}kb
                 </Text>
               </View>
             </View>
             <TouchableOpacity
               onPress={handleDelete}
-              style={tw`p-2`}
+              style={tw`p-2 flex-row items-center`}
             >
+                <TrashIcon />
               {/* <Icon name="delete" size={20} color="#EF4444" /> */}
+              <Text  style={tw`text-dark-error9 text-sm-bold`}>  Delete</Text>
             </TouchableOpacity>
           </View>
         )}
--- src/lib/initials.ts
+++ src/lib/initials.ts
@@ -17,21 +17,21 @@ const getInitials = (input: string = '', num: number = 1): string => {
 
     // For a single word, take only the first letter
     if (words.length === 1 && num === 1) {
-        result = words[0][0].toUpperCase()
+        result = String(words[0]?.[0]).toUpperCase()
     }
 
     if (words.length === 1 && num === 2) {
-        result = words[0][0].toUpperCase() + words[0][1].toUpperCase()
+        result =  String(words[0]?.[0]).toUpperCase() +  String(words[0]?.[1]).toUpperCase()
     }
 
     // For two words, take the first letter of each word
     if (words.length === 2) {
-        result = words[0][0].toUpperCase() + words[1][0].toUpperCase()
+        result =  String(words[0]?.[0]).toUpperCase() +  String(words[1]?.[0]).toUpperCase()
     }
 
     //  For three or more words, take the first and second letter of the first two words
     if (words.length > 2) {
-        result = words[0][0].toUpperCase() + words[1][0].toUpperCase()
+        result =  String(words[0]?.[0]).toUpperCase() +  String(words[1]?.[0]).toUpperCase()
     }
 
     return result
